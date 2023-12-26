73267c84b4f62a3bf0ad615d543b72f900a5d88f
xiayuanOvO <xiayuan.ovo@gmail.com>
2023-12-26 18:25:54 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,56 @@
+import { defaultTheme } from 'vuepress'
+
+export default {
+  base: "/Modernia/",
+  lang: 'zh-CN',
+  title: 'Modernia Wiki',
+  description: '基于botpy的娱乐QQ机器人。',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '指南',
+        link: '/guide/',
+      },
+      {
+        text: '设计模式',
+        link: '/designPatterns/',
+      },
+    ],
+    // 侧边栏对象
+    // 不同子路径下的页面会使用不同的侧边栏
+    sidebar: {
+      '/network/': [
+        {
+          text: '网络',
+          children: [
+              {
+                text: '一张图帮你看懂，在浏览器输入网址回车后，都发生了什么？',
+                link: '/network/一张图帮你看懂，在浏览器输入网址回车后，都发生了什么？.md',
+              },
+              {
+                text: '在Jekyll中创建一个新的列表页面',
+                link: '/network/在Jekyll中创建一个新的列表页面.md',
+              }
+            ],
+        },
+      ],
+      '/designPatterns/': [
+        {
+          text: '设计模式',
+          collapsible: true,
+          children: [
+              {
+                text: '单例模式--我的机器人女友',
+                link: '/designPatterns/单例模式--我的机器人女友.md',
+              }
+          ],
+        },
+      ],
+    },    
+
+  }),
+}

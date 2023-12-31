f1025bac8aa4982d10f63fa0a230e3b5ded84a2a
xiayuanOvO <xiayuan.ovo@gmail.com>
2023-12-31 14:52:37 +0800

    更新内容

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,39 +19,15 @@ export default defineUserConfig({
       { text: 'FAQ', link: '/faq/' }
     ],
     backToHome: '返回首页',
-    editLinkText: '编辑此页'
+    lastUpdated: true,
+    lastUpdatedText: '上一次编辑',
+    editLinkText: '编辑此页',
+    contributors: true,
+    tip: "提示",
+    warning: "注意",
+    danger: "警告",
   }),
   plugins: [
     backToTopPlugin(),
   ],
 })
-
-// export default defineUserConfig({
-  
-//   title: 'Modernia Wiki',
-//   description: '基于botpy的娱乐QQ机器人。',
-//   base: "/Modernia-docs/",
-//   lang: 'zh-cn',
-  
-//   theme: defaultTheme({
-//     lang: 'zh-CN',
-//     repo: 'xiayuanOvO/Modernia',
-//     docsRepo: 'ishkong/go-cqhttp-docs',
-//     docsBranch: 'main',
-//     docsDir: 'docs',
-//     navbar: [
-//       { text: '首页', link: '/' },
-//       { text: '指南', children: ['/guide/', '/guide/start.md'] },
-//       { text: 'FAQ', link: '/faq/' }
-//     ],
-//     editLinkText: "编辑此页",
-//     lastUpdated: true,
-//     lastUpdatedText: '上一次编辑',
-//     contributors: true,
-//     tip: "提示",
-//     warning: "注意",
-//     danger: "警告",
-//     backToHome: "返回首页",
-//     sidebar: "auto",
-//   }),
-// })
